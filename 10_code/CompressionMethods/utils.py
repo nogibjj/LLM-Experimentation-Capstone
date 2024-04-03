94b356a6764c509fee1030f4eea5977ee170e119
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2024-04-03 02:34:36 +0000

    minor code changes to utils.py

--- 10_code/CompressionMethods/utils.py
+++ 10_code/CompressionMethods/utils.py
@@ -14,7 +14,7 @@ def load_data_hf(dataset_id = None, dataset_subid = None):
         dataset_id (str): the dataset stored from HuggingFace. If this is none, you can specify your custom dataset in the tokenizer class.
         dataset_subid (str): the specific sub dataset within the corpus of data in huggingface
     Returns:
-        dataset (tensor): dataset loaded from HuggingFace
+        dataset (tensor): dataset object
     """
     if not dataset_subid:
         dataset = load_dataset(dataset_id)
@@ -31,7 +31,6 @@ def get_model_size(model):
     Returns:
         dataset (tensor): dataset loaded from HuggingFace
     """
-
     param_size = 0
     for param in model.parameters():
         param_size += param.nelement() * param.element_size()
@@ -45,20 +44,41 @@ def get_model_size(model):
     return model_size
 
 def multi_label_metrics_eval(predictions, labels, threshold=0.5):
-        """Helper function to perform inferences using the model."""
-        y_pred = []
-        for inner_list in predictions:
-            new_inner_list = [1 if value > threshold else 0 for value in list(inner_list)]
-            y_pred.append(new_inner_list)
+    """
+    Function to the multi-label accuracy of a model given a list of predictions and labels
+    Params:
+        predictions (list): A list of predictions made by the model
+        labels (list): The corresponding list of true labels 
+        threshold (int): The probability threshold for classifying an entry with a specific class
+    Returns:
+        accuracy (int): The accuracy score of the model
+    """
+    y_pred = []
+    for inner_list in predictions:
+        new_inner_list = [1 if value > threshold else 0 for value in list(inner_list)]
+        y_pred.append(new_inner_list)
 
-        # finally, compute metrics
-        accuracy = accuracy_score(labels, y_pred)
-        # return as dictionary
-        return accuracy
+    # finally, compute metrics
+    accuracy = accuracy_score(labels, y_pred)
+    # return as dictionary
+    return accuracy
 
 def evaluate_heegyu_augsec(model, tokenizer, dataset, device="cuda", num_samples=1000):
+    """
+    Function to evaluate the performace of a model that is fine-tuned on the heegyu/augsec dataset on huggingface
+    Params:
+        model (transformers.model): The model being evaluated
+        tokenizer (transformer.tokenzier): The corresponding tokenizer of the model
+        dataset (datasets): The dataset object that contains the heegyu/augsec data
+        device (str): Device cuda/cpu that denotes the machine to run the evaluation on
+        num_samples (int): The number of samples to run the evaluation for
+    Returns:
+        result (dict): Returns a dict containing
+            1. Mean Inference Time
+            2. Accuracy
+    """
     label_map = {"Question":0, "Restatement or Paraphrasing":1, "Reflection of feelings":2, "Self-disclosure":3, "Affirmation and Reassurance":4,
-        "Providing Suggestions":5, "Information":6, "Others":7}
+                "Providing Suggestions":5, "Information":6, "Others":7}
 
     x = []
     y_true = []
@@ -77,7 +97,7 @@ def evaluate_heegyu_augsec(model, tokenizer, dataset, device="cuda", num_samples
     y_pred = []
     times = []
     for current_x in input:
-        inputs = tokenizer(current_x, return_tensors="pt").to("cuda")
+        inputs = tokenizer(current_x, return_tensors="pt").to(device)
         start_time = time.time()
         logits = model(**inputs).logits.softmax(-1)
         end_time = time.time()
@@ -90,7 +110,18 @@ def evaluate_heegyu_augsec(model, tokenizer, dataset, device="cuda", num_samples
     return {"mean_time":mean_time, "accuracy":accuracy}
 
 def evaluate_sem_eval_2018_task_1_dataset(model, tokenizer, dataset, device="cuda"):
-    """Helper function to evaluate the inference time and performance of finetuned model."""
+    """
+    Function to evaluate the performace of a model that is fine-tuned on the sem_eval_2018_task_1/subtask5.english dataset on huggingface
+    Params:
+        model (transformers.model): The model being evaluated
+        tokenizer (transformer.tokenzier): The corresponding tokenizer of the model
+        dataset (datasets): The dataset object that contains the sem_eval_2018_task_1/subtask5.english data
+        device (str): Device cuda/cpu that denotes the machine to run the evaluation on
+    Returns:
+        result (dict): Returns a dict containing
+            1. Mean Inference Time
+            2. Accuracy
+    """
     def convert_dict_to_labels(dictionary):
         return [int(dictionary[key]) for key in dictionary if key not in ['ID', 'Tweet']]
 
